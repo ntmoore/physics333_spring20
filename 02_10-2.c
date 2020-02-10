496ad18be554ce56233a54f90b90ea88353fe1a5
Nathan Moore <ntmoore@gmail.com>
2020-02-09 22:35:41 -0600

    int and float division
    Example shows the difference between int div/mod and float division

---
+++ 02_10-2.c
@@ -0,0 +1,19 @@
+void setup() {
+  Serial.begin(9600);
+  int i;
+  char line[60];
+
+  for (i = 0; i < 30; i+=3) {
+    float x = i / 10.0;
+    sprintf(line,
+            "%d\t%d\t%d\t",
+            i, i / 10, i % 10
+           );
+    Serial.print(line);
+    Serial.println(x);
+  }
+}
+
+void loop() {
+
+}
