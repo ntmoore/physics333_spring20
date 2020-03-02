b5017303efb21db1e2a1d7d9b4b1221704f18b6a
Nathan Moore <ntmoore@gmail.com>
2020-03-02 10:04:21 -0600

    Create 03_02_read_and_average.c

---
+++ 03_02_read_and_average.c
@@ -0,0 +1,15 @@
+int read_time_ms;
+int num_reads;
+float x_sum, x_square_sum;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read as much as possible within a set time interval (read_time_ms)
+  
+  // compute average and uncertainty of the read during this interval
+  
+  // print out results
+}
